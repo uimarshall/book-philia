1ab0554f588ad67cbab9000f9cb5e8ebe4399d17
uimarshall <uimarshall@gmail.com>
2020-10-28 21:53:21 +0100

    Set up parcel bundler

---
+++ src/greet.js
@@ -0,0 +1,6 @@
+
+const greet = (greetings, name)=>{
+    return `${greetings}, ${name}`
+}
+
+export {greet}
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import {greet} from './greet'
+document.querySelector('h1').textContent = greet('Hi guys', 'Brady')
